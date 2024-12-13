c92be6ac34798b5d5fec62ac6afecf49bf3af44d
SerUKalinin <serukalinin@gmail.com>
2024-12-13 17:29:51 +0300

    AchievementFilterDto

---
+++ src/main/java/faang/school/achievement/dto/achievement/AchievementFilterDto.java
@@ -0,0 +1,13 @@
+package faang.school.achievement.dto.achievement;
+
+
+import faang.school.achievement.model.Rarity;
+import lombok.Builder;
+
+@Builder
+public record AchievementFilterDto(
+    String title,
+    String description,
+    Rarity rarity
+) {
+}
