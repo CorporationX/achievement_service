e9f4c85a2eee406b0bef51475b3a46b8d0c39e73
Muhammadsolih Abdugafforov <salikhdev@gmail.com>
2025-02-12 16:38:16 +0500

    Получение достижения "Блогер" (cross review) 1.2v

--- src/main/java/faang/school/achievement/config/redis/RedisConfig.java
+++ src/main/java/faang/school/achievement/config/redis/RedisConfig.java
@@ -28,7 +28,7 @@ public class RedisConfig {
     }
 
     @Bean
-    public RedisTemplate<String, String> achievementRedisTemplate(RedisConnectionFactory connectionFactory) {
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, String> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
