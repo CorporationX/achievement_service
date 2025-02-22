2ceb040215ffb357c388d0830c7424316c520411
rostex <rostex2@yandex.ru>
2025-02-22 21:11:55 +0300

    Revert "Исправлены тесты"
    This reverts commit 1eed395e

--- src/test/java/faang/school/achievement/cache/AchievementCacheTest.java
+++ src/test/java/faang/school/achievement/cache/AchievementCacheTest.java
@@ -11,9 +11,6 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
@@ -55,36 +52,13 @@ public class AchievementCacheTest {
     }
 
     @Test
-    void testWarmUpCache() {
-        Achievement achievement1 = Achievement.builder().title("Test1").build();
-        Achievement achievement2 = Achievement.builder().title("Test2").build();
-        Achievement achievement3 = Achievement.builder().title("Test3").build();
-        Achievement achievement4 = Achievement.builder().title("Test4").build();
-        Achievement achievement5 = Achievement.builder().title("Test5").build();
-        Achievement achievement6 = Achievement.builder().title("Test6").build();
-
+    public void testAchievementCacheWarmUp() throws Exception {
+        Achievement achievement = Achievement.builder().title("Test").build();
         Mockito.when(cacheManager.getCache("achievementTitle")).thenReturn(cache);
-
-        Pageable firstPage = PageRequest.of(0, 5);
-        Mockito.when(achievementRepository.findAll(firstPage))
-                .thenReturn(new PageImpl<>(List.of(achievement1, achievement2, achievement3, achievement4, achievement5)));
-
-        Pageable secondPage = PageRequest.of(1, 5);
-        Mockito.when(achievementRepository.findAll(secondPage))
-                .thenReturn(new PageImpl<>(List.of(achievement6)));
+        Mockito.when(achievementRepository.findAll()).thenReturn(List.of(achievement));
 
         achievementCache.warmUpCache();
 
-        Mockito.verify(cache, Mockito.times(1)).put("Test1", achievement1);
-        Mockito.verify(cache, Mockito.times(1)).put("Test2", achievement2);
-        Mockito.verify(cache, Mockito.times(1)).put("Test3", achievement3);
-        Mockito.verify(cache, Mockito.times(1)).put("Test4", achievement4);
-        Mockito.verify(cache, Mockito.times(1)).put("Test5", achievement5);
-        Mockito.verify(cache, Mockito.times(1)).put("Test6", achievement6);
-
-
-        Mockito.verify(cache, Mockito.times(6)).put(Mockito.anyString(), Mockito.any());
+        Mockito.verify(cache).put("Test", achievement);
     }
-
-
 }
