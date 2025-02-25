9b96cdbda50761fa21bf5405a2a206d7421e531b
senty <senty1844@gmail.com>
2025-02-25 23:15:43 +0300

    Добавил логику кеширования достижений, изменил репозиторий с crud на JPA, изменил коллекции в сущностях с List на Set

---
+++ src/main/java/faang/school/achievement/cache/AchievementCache.java
@@ -0,0 +1,31 @@
+package faang.school.achievement.cache;
+
+import faang.school.achievement.model.Achievement;
+import faang.school.achievement.repository.AchievementRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor
+public class AchievementCache {
+    private final AchievementRepository achievementRepository;
+    private Map<String, Achievement> achievements;
+
+    public Achievement get(String title) {
+        return achievements.get(title);
+    }
+
+    @PostConstruct
+    @Transactional
+    public void init() {
+        achievements = achievementRepository.findAllWithLazyCollections()
+                .stream()
+                .collect(Collectors.toMap(Achievement::getTitle, Function.identity()));
+    }
+}
