e39b593550e415455bcc17934dcf9724dc02e0e9
Natalya Vasileva <boryantheone228@yandex.ru>
2025-03-08 11:53:44 +0300

    BJS2-54527 Получение достижения "Организатор"

--- src/main/resources/db/changelog/changeset/achievement_setup_V003.sql
+++ src/main/resources/db/changelog/changeset/achievement_setup_V003.sql
@@ -1,3 +1,3 @@
 INSERT INTO achievement (title, description, rarity, points, created_at, updated_at)
 VALUES
-    ('ORGANIZER', 'For 100 invited users to the project', 2, 20, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    ('ORGANIZER', 'For 100 invited users to the project', 2, 20, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
