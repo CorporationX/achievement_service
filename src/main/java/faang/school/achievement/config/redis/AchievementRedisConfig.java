620db093cb6e7ac22c4a7f558f216e88b1642903
Artem Kopanev <artemkopanev757@gmail.com>
2025-03-22 20:11:33 +0300

    BJS2-54530

---
+++ src/main/java/faang/school/achievement/config/redis/AchievementRedisConfig.java
@@ -0,0 +1,17 @@
+package faang.school.achievement.config.redis;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.listener.ChannelTopic;
+
+@Configuration
+public class AchievementRedisConfig extends RedisConfig {
+    @Value("${spring.data.redis.channel.achievement}")
+    private String channelTopic;
+
+    @Bean
+    ChannelTopic topic() {
+        return new ChannelTopic(channelTopic);
+    }
+}
