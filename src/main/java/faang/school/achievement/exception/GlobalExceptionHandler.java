5832922203a6bbf29a407f89c5e40ed215d7330e
Ilyas Azhdar <azhdar902@gmail.com>
2025-04-10 16:20:46 +0500

    added GlobalExceptionHandler

---
+++ src/main/java/faang/school/achievement/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package faang.school.achievement.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EmptyFilterException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleEmptyFilterException(EmptyFilterException ex) {
+        log.error("EmptyFilterException occurred: {}", ex.getMessage());
+        return ex.getMessage();
+    }
+}
