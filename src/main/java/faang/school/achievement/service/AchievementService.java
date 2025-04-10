c267885bfc26422d455c40b05c25bbcd6cbfc7f0
Ilyas Azhdar <azhdar902@gmail.com>
2025-04-10 15:19:18 +0500

    init base structure

---
+++ src/main/java/faang/school/achievement/service/AchievementService.java
@@ -0,0 +1,16 @@
+package faang.school.achievement.service;
+
+import faang.school.achievement.repository.AchievementProgressRepository;
+import faang.school.achievement.repository.AchievementRepository;
+import faang.school.achievement.repository.UserAchievementRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AchievementService {
+
+    private final AchievementRepository achievementRepository;
+    private final UserAchievementRepository userAchievementRepository;
+    private final AchievementProgressRepository achievementProgressRepository;
+}
