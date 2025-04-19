14df0410ca1da94208120bba622c1508c7002d95
Dmitry Rogulkin <rogulkind@yandex.ru>
2025-04-19 15:08:31 +0500

    https://faang-school.atlassian.net/browse/BJS2-65567
    Start task

---
+++ src/main/java/faang/school/achievement/service/AchievementCache.java
@@ -0,0 +1,31 @@
+package faang.school.achievement.service;
+
+import faang.school.achievement.model.Achievement;
+import faang.school.achievement.repository.AchievementRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class AchievementCache {
+    private final Map<String, Achievement> achievementCash = new HashMap<>();
+    private final AchievementRepository repository;
+
+    @PostConstruct
+    private void init() {
+        repository.findAll().forEach(achievement -> {
+            achievementCash.put(achievement.getTitle(), achievement);
+        });
+    }
+
+    private Achievement get(String title) {
+
+        return new Achievement();
+    }
+
+}
+
