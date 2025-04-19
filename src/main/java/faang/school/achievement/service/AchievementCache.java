f13dff56fdf1eafbfa45123d7a1888f001062e45
Dmitry Rogulkin <rogulkind@yandex.ru>
2025-04-19 15:13:48 +0500

    https://faang-school.atlassian.net/browse/BJS2-65567
    code style

--- src/main/java/faang/school/achievement/service/AchievementCache.java
+++ src/main/java/faang/school/achievement/service/AchievementCache.java
@@ -12,18 +12,18 @@ import java.util.Map;
 @Service
 @RequiredArgsConstructor
 public class AchievementCache {
-    private final Map<String, Achievement> achievementCash = new HashMap<>();
+    private final Map<String, Achievement> achievementCache = new HashMap<>();
     private final AchievementRepository repository;
 
     @PostConstruct
     private void init() {
         repository.findAll().forEach(achievement -> {
-            achievementCash.put(achievement.getTitle(), achievement);
+            achievementCache.put(achievement.getTitle(), achievement);
         });
     }
 
-    private Achievement get(String title) {
-        return achievementCash.get(title);
+    public Achievement get(String title) {
+        return achievementCache.get(title);
     }
 
 }
