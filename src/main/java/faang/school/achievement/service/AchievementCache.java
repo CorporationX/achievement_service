47aa4cd3d56ae080730d81168a5bd2bdfe94a50c
Dmitry Rogulkin <rogulkind@yandex.ru>
2025-04-19 15:27:11 +0500

    https://faang-school.atlassian.net/browse/BJS2-65567
    code style

--- src/main/java/faang/school/achievement/service/AchievementCache.java
+++ src/main/java/faang/school/achievement/service/AchievementCache.java
@@ -4,11 +4,13 @@ import faang.school.achievement.model.Achievement;
 import faang.school.achievement.repository.AchievementRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class AchievementCache {
@@ -20,6 +22,7 @@ public class AchievementCache {
         repository.findAll().forEach(achievement -> {
             achievementCache.put(achievement.getTitle(), achievement);
         });
+        log.info("Кеш достижений заполнен.");
     }
 
     public Achievement get(String title) {
