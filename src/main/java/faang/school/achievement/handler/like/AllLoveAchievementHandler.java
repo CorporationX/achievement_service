001d1c899439c29c54dddeab8b64867212e19b9b
Артемий <xkmitx@gmail.com>
2025-04-30 13:49:02 +0300

    сделал отступ

--- src/main/java/faang/school/achievement/handler/like/AllLoveAchievementHandler.java
+++ src/main/java/faang/school/achievement/handler/like/AllLoveAchievementHandler.java
@@ -14,6 +14,7 @@ public class AllLoveAchievementHandler extends LikeEventHandler {
 
     @Value("${app.achievements.allLove.name}")
     private String achievementName;
+
     @Value("${app.achievements.allLove.requiredLikes}")
     private int requiredLikes;
     public AllLoveAchievementHandler(AchievementServiceImpl achievementService) {
