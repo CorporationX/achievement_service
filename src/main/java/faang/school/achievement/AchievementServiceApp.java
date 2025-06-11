036680c541c914f5f0eefe4b4671bac3b2e29e3a
Vladimir Levshin <levshin@intervolga.ru>
2025-06-11 10:48:47 +0300

    Added ci.

--- src/main/java/faang/school/achievement/AchievementServiceApp.java
+++ src/main/java/faang/school/achievement/AchievementServiceApp.java
@@ -4,14 +4,18 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableFeignClients("faang.school.achievement.client")
 @EnableAsync
+@ConfigurationPropertiesScan
+@EnableScheduling
 @EnableRetry
 public class AchievementServiceApp {
     public static void main(String[] args) {
