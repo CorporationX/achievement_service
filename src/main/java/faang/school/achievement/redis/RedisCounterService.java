f0feb5c35cad19289c4cac6b13c98570314f688a
Kirill <kirill551@yandex.ru>
2025-06-21 22:30:36 +0200

    "Добавить аннотации @Retryable и @Recover для отказоустойчивости при работе с Redis"

--- src/main/java/faang/school/achievement/redis/RedisCounterService.java
+++ src/main/java/faang/school/achievement/redis/RedisCounterService.java
@@ -3,7 +3,11 @@ package faang.school.achievement.redis;
 import faang.school.achievement.model.AchievementCode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.RedisConnectionFailureException;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 
 @Slf4j
 public abstract class RedisCounterService {
@@ -19,18 +23,33 @@ public abstract class RedisCounterService {
         this.stringRedisTemplate = stringRedisTemplate;
     }
 
+    @Retryable(
+            retryFor = RedisConnectionFailureException.class,
+            maxAttempts = 3,
+            backoff = @Backoff(delay = 2000, multiplier = 2)
+    )
     public Long incrementCounter(AchievementCode achievementCode, long userId) {
         log.info("Increment counter title: {}, userId = {}", achievementCode.getName(), userId);
         String key = achievementKeyBuilder.userProgress(achievementCode.getName(), userId);
         return redisCounterTemplate.opsForValue().increment(key, 1);
     }
 
+    @Retryable(
+            retryFor = RedisConnectionFailureException.class,
+            maxAttempts = 3,
+            backoff = @Backoff(delay = 2000, multiplier = 2)
+    )
     public void decrementCounter(AchievementCode achievementCode, long userId, long delta) {
         log.info("Decrement counter title: {}, userId = {}", achievementCode.getName(), userId);
         String key = achievementKeyBuilder.userProgress(achievementCode.getName(), userId);
         redisCounterTemplate.opsForValue().decrement(key, delta);
     }
 
+    @Retryable(
+            retryFor = RedisConnectionFailureException.class,
+            maxAttempts = 3,
+            backoff = @Backoff(delay = 2000, multiplier = 2)
+    )
     public void assignAchievement(AchievementCode achievementCode, long userId) {
         log.info("Updating a cache with a completed achievement for a user, title: {}, userId = {}", achievementCode.getName(), userId);
         String key = achievementKeyBuilder.assignAchievement(userId);
@@ -38,12 +57,22 @@ public abstract class RedisCounterService {
         removeAchievementProgressTracking(achievementCode, userId);
     }
 
+    @Retryable(
+            retryFor = RedisConnectionFailureException.class,
+            maxAttempts = 3,
+            backoff = @Backoff(delay = 2000, multiplier = 2)
+    )
     public void removeAchievementProgressTracking(AchievementCode achievementCode, long userId) {
         log.info("Removing tracking of the achievement to a user, title: {}, userId = {}", achievementCode.getName(), userId);
         String key = achievementKeyBuilder.userProgress(achievementCode.getName(), userId);
         stringRedisTemplate.delete(key);
     }
 
+    @Retryable(
+            retryFor = RedisConnectionFailureException.class,
+            maxAttempts = 3,
+            backoff = @Backoff(delay = 2000, multiplier = 2)
+    )
     public Boolean checkIfAchievementAssigned(AchievementCode achievementCode, long userId) {
         log.info("Checking if an achievement has been assigned to a user, title: {}, userId = {}",
                 achievementCode.getName(), userId);
@@ -54,5 +83,20 @@ public abstract class RedisCounterService {
             return isAssigned;
         }
         return false;
+    }
+
+    @Recover
+    public void recover(RedisConnectionFailureException e,
+                        AchievementCode code,
+                        long userId) {
+        log.error("Method failed with params: {}, userId={}", code, userId, e);
+    }
+
+    @Recover
+    public void recoverDecrement(RedisConnectionFailureException e,
+                                 AchievementCode code,
+                                 long userId,
+                                 long delta) {
+        log.error("[DECREMENT] Retry failed: {}, userId={}, delta={}", code, userId, delta, e);
     }
 }
