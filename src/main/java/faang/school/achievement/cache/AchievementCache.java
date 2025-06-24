36772241b1f40092cc007023aa344d53f2e577aa
Jegor <gaanjara3@gmail.com>
2025-06-24 18:49:51 +0300

    [UPDATE] BJS2-77710 Закончил задачу.

--- src/main/java/faang/school/achievement/cache/AchievementCache.java
+++ src/main/java/faang/school/achievement/cache/AchievementCache.java
@@ -1,33 +1,40 @@
 package faang.school.achievement.cache;
 
+import faang.school.achievement.dto.AchievementDto;
+import faang.school.achievement.mapper.AchievementMapper;
 import faang.school.achievement.model.Achievement;
 import faang.school.achievement.repository.AchievementRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Component
 @RequiredArgsConstructor
 public class AchievementCache {
     private static final String CACHE_KEY_PREFIX = "achievement:";
     private final AchievementRepository achievementRepository;
-    private RedisTemplate<String, Object> redisTemplate;
+    private final Map<String, AchievementDto> cache = new ConcurrentHashMap<>();
+    private final AchievementMapper achievementMapper;
 
     @PostConstruct
-    private void initCache() {
+    @Transactional
+    private void init() {
         for (Achievement achievement : achievementRepository.findAll()) {
-            redisTemplate.opsForValue().set(CACHE_KEY_PREFIX + achievement.getTitle(), achievement);
+            AchievementDto achievementDto = achievementMapper.toDto(achievement);
+            cache.put(CACHE_KEY_PREFIX + achievement.getTitle(), achievementDto);
         }
     }
 
-    public Optional<Achievement> get(String title) {
+    public Optional<AchievementDto> get(String title) {
         String redisKey = CACHE_KEY_PREFIX + title;
-        Achievement achievement = (Achievement) redisTemplate.opsForValue().get(redisKey);
+        AchievementDto achievementDto = cache.get(redisKey);
 
-        if (achievement != null ) return Optional.of(achievement);
+        if (achievementDto != null) return Optional.of(achievementDto);
         return Optional.empty();
     }
 }
