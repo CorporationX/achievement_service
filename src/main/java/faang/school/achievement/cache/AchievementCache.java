671673b1cc31411a587e24a2d9feaff28a64fa1f
Jegor <gaanjara3@gmail.com>
2025-06-26 20:17:24 +0300

    [UPDATE] BJS2-77710 Поправил помарки.

--- src/main/java/faang/school/achievement/cache/AchievementCache.java
+++ src/main/java/faang/school/achievement/cache/AchievementCache.java
@@ -1,40 +1,10 @@
 package faang.school.achievement.cache;
 
 import faang.school.achievement.dto.AchievementDto;
-import faang.school.achievement.mapper.AchievementMapper;
-import faang.school.achievement.model.Achievement;
-import faang.school.achievement.repository.AchievementRepository;
-import jakarta.annotation.PostConstruct;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
 
-@Component
-@RequiredArgsConstructor
-public class AchievementCache {
-    private static final String CACHE_KEY_PREFIX = "achievement:";
-    private final AchievementRepository achievementRepository;
-    private final Map<String, AchievementDto> cache = new ConcurrentHashMap<>();
-    private final AchievementMapper achievementMapper;
+public interface AchievementCache {
 
-    @PostConstruct
-    @Transactional
-    private void init() {
-        for (Achievement achievement : achievementRepository.findAll()) {
-            AchievementDto achievementDto = achievementMapper.toDto(achievement);
-            cache.put(CACHE_KEY_PREFIX + achievement.getTitle(), achievementDto);
-        }
-    }
-
-    public Optional<AchievementDto> get(String title) {
-        String redisKey = CACHE_KEY_PREFIX + title;
-        AchievementDto achievementDto = cache.get(redisKey);
-
-        if (achievementDto != null) return Optional.of(achievementDto);
-        return Optional.empty();
-    }
+    Optional<AchievementDto> get(String title);
 }
---
+++ src/main/java/faang/school/achievement/cache/AchievementCacheImpl.java
@@ -0,0 +1,33 @@
+package faang.school.achievement.cache;
+
+import faang.school.achievement.dto.AchievementDto;
+import faang.school.achievement.mapper.AchievementMapper;
+import faang.school.achievement.model.Achievement;
+import faang.school.achievement.repository.AchievementRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+@RequiredArgsConstructor
+public class AchievementCacheImpl implements AchievementCache {
+    private final AchievementRepository achievementRepository;
+    private final Map<String, AchievementDto> cache = new ConcurrentHashMap<>();
+    private final AchievementMapper achievementMapper;
+
+    @PostConstruct
+    private void init() {
+        for (Achievement achievement : achievementRepository.findAll()) {
+            AchievementDto achievementDto = achievementMapper.toDto(achievement);
+            cache.put(achievement.getTitle(), achievementDto);
+        }
+    }
+
+    public Optional<AchievementDto> get(String title) {
+        return Optional.ofNullable(cache.get(title));
+    }
+}
