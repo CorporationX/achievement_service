eea7b6ff0725a41419581559aff0a4c4c8c34c7a
Vladimir Ekgardt <dj.estet@yandex.ru>
2025-07-28 13:47:58 +0300

    BJS2-77069 Получение достижения "Лидер мнений"

--- src/main/java/faang/school/achievement/achievement_handler/NiceGuyAchievementHandler.java
+++ src/main/java/faang/school/achievement/achievement_handler/NiceGuyAchievementHandler.java
@@ -1,13 +1,12 @@
 package faang.school.achievement.achievement_handler;
 
-import faang.school.achievement.dto.event.RecommendationEvent;
 import faang.school.achievement.repository.AchievementRepository;
 import faang.school.achievement.service.AchievementService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-public class NiceGuyAchievementHandler extends AbstractEventHandler<RecommendationEvent> {
+public class NiceGuyAchievementHandler extends AbstractEventHandler {
     public NiceGuyAchievementHandler(AchievementService service,
                                      @Value("${achievement.titles.nice-guy}") String title,
                                      @Value("${achievement.required-progress.nice-guy}") int requiredProgress,
---
+++ src/main/java/faang/school/achievement/dto/PostEvent.java
@@ -0,0 +1,6 @@
+package faang.school.achievement.dto;
+
+public record PostEvent(
+        Long receiverId
+) {
+}
